2337d93c6e1b63d2e879b1649e2181d037984ffa
brunota20 <brunot.anjos@gmail.com>
2023-04-17 00:09:47 -0300

    distâncias manhattan e simples feitas, com erro

--- trab1_bruno.py
+++ trab1_bruno.py
@@ -5,7 +5,7 @@ objetivo = [[1,2,3],[4,5,6],[7,8,0]]
 
 entrada1 = [[6,7,5],[1,2,3],[0,4,8]]
 entrada2 = [[3,1,8],[5,6,2],[7,4,0]]
-entrada_teste = [[1,2,3],[4,0,6],[7,5,8]]
+entrada_teste = [[1,0,2],[5,6,3],[4,7,8]]
 
 regras = {(0,0):[(0,1),(1,0)], (0,1):[(0,0),(0,2),(1,1)], (0,2):[(0,1),(1,2)], 
           (1,0):[(0,0),(1,1),(2,0)], (1,1):[(0,1),(1,0),(1,2),(2,1)], (1,2):[(0,2),(1,1),(2,2)], 
@@ -38,12 +38,12 @@ def troca_zero(entrada, posicao_zero, regras_possiveis, lista_visto, lista_abert
     return lista_visto, lista_aberto, lista_movimentos
 
 
-def busca_em_largura(entrada):
+def busca_em_largura(entrada, heuristica_var = "manhattan"):
     lista_aberto = [entrada]
     lista_visto = []
     lista_movimentos = []
     count = 0
-    
+
     while lista_aberto:
         nodo_atual = lista_aberto.pop(0)
         if nodo_atual == objetivo:
@@ -52,8 +52,61 @@ def busca_em_largura(entrada):
         posicao_zero = acha_zero(nodo_atual)
         regras_possiveis = acha_regra(posicao_zero)
         lista_visto, lista_aberto, lista_movimentos = troca_zero(nodo_atual, posicao_zero, regras_possiveis, lista_visto, lista_aberto, lista_movimentos)
+        if heuristica_var == "manhattan" or heuristica_var == "simples":
+            lista_aberto = heuristica_funcao(lista_aberto, heuristica_var)
         count += 1
     return False
+
+
+def qtd_fora_do_lugar(entrada, objetivo):
+    count = 0
+    for i in range(3):
+        for j in range(3):
+            if entrada[i][j] != objetivo[i][j] and entrada[i][j] != 0:
+                count += 1
+    return count
+
+
+def manhattan_distance(matrix1, matrix2):
+    pos1 = acha_zero(matrix1)
+    pos2 = acha_zero(matrix2)
+    
+    # Calculando a soma das distâncias de Manhattan
+    distance = 0
+    for i in range(3):
+        for j in range(3):
+            value1 = matrix1[i][j]
+            if value1 != 0:
+                # Encontrando a posição do elemento correspondente na segunda matriz
+                pos2_value = [(k, l) for k in range(3) for l in range(3) if matrix2[k][l] == value1][0]
+                # Calculando a distância de Manhattan
+                distance += abs(i - pos2_value[0]) + abs(j - pos2_value[1])
+    
+    return distance + abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1]) # Adicionando a distância de Manhattan entre os elementos "0" das duas matrizes
+    
+
+def find_value(matrix, value):
+  for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+      if matrix[i][j] == value:
+        return (i, j)
+
+
+def heuristica_funcao(matrix, heuristica = "manhattan", objetivo = [[1,2,3],[4,5,6],[7,8,0]]):
+    lista_pontuacao = []
+    if heuristica == "manhattan":
+        for filho in matrix:
+            lista_pontuacao.append(manhattan_distance(filho, objetivo))
+    if heuristica == "simples":
+        for filho in matrix:
+            lista_pontuacao.append(qtd_fora_do_lugar(filho, objetivo))
+
+    indice = lista_pontuacao.index(min(lista_pontuacao))
+
+    if heuristica == "busca_largura":
+        return 0
+
+    return [matrix[indice]]
 
 
 def movimentos(lista_movimentos, entrada):
@@ -72,9 +125,9 @@ def movimentos(lista_movimentos, entrada):
 
 
 init = time.time()
-busca_larg = busca_em_largura(entrada1)
+busca_larg = busca_em_largura(entrada_teste, "simples")
 end = time.time()
-lista_mov = movimentos(busca_larg, entrada1)
+lista_mov = movimentos(busca_larg, entrada_teste)
 print(lista_mov)
 print(f"Quantidade de movimentos (contanto com a entrada): ", len(lista_mov))
 
